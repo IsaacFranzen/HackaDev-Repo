7d32ef3cf843967fee6b0ea11269d1ec27d3c6cd
Gustavo Vaz <104520068+Gustavonuva@users.noreply.github.com>
2023-04-11 17:35:42 -0300

    fix: correção

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles/globals.js";
 
-
 import Home from "./pages/Home";
 import Cadastro from "./pages/Cadastro.js";
-import Layout from "./components/Layout/Layout";
 import TransacaoPage from "./pages/TransacaoPage.js";
 import SaldoPage from "./pages/SaldoPage.js";
 import LogadoPage from "./pages/LogadoPage.js";
@@ -17,14 +15,11 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/cadastro" element={<Cadastro />} />
-          <Route exact path="/" element={<Layout />} />
-          <Route path="/area-logada" element={<LogadoPage/>}/>
-          <Route path="/transacoes" element= {<TransacaoPage />} />
-          <Route path="/saldo" element= {<SaldoPage />} />
+          <Route path="/area-logada" element={<LogadoPage />} />
+          <Route path="/transacoes" element={<TransacaoPage />} />
+          <Route path="/saldo" element={<SaldoPage />} />
         </Routes>
       </Router>
-        
-      
     </>
   );
 }

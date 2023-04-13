d48d015e25b76653093fe3ac8f7e8aa7e4a15466
Guilherme Teixeira <guilherme_oliveira44@hotmail.com>
2023-04-13 20:30:50 -0300

    comecando modelagem entitade endereco

---
+++ backend/src/TECBank.Backend.API/Domain/Model/Endereço.cs
@@ -0,0 +1,13 @@
+using TECBank.Backend.Domain.Model;
+
+public class Endereco : BaseEntity
+{
+    public string Logradouro { get; set; } 
+    public string Numero { get; set; }
+    public string Complemento { get; set; }
+    public string Bairro { get; set; }
+    public string Cep { get; set; }
+    public string Cidade { get; set; }
+    public string Estado { get; set; }
+
+}
--- backend/src/TECBank.Backend.API/Repository/DataContext/TecBankContext.cs
+++ backend/src/TECBank.Backend.API/Repository/DataContext/TecBankContext.cs
@@ -1,8 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 
+
 namespace TECBank.Backend.Repository.DataContext;
 
 public class TecBankContext : DbContext
 {
     public TecBankContext(DbContextOptions<TecBankContext> options) { }
+    public DbSet<Endereco> Enderecos { get; set; }  
+
 }
